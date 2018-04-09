cec50c9452b570e619e5381b1b8203c11cd31edd
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2018-04-08 21:25:35 -0300

    define password undefined before return data

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -12,6 +12,7 @@ router.post('/register', async (req, res) => {
       return res.status(400).send({ error: 'User already exists' });
 
     const user = await User.create(req.body);
+    user.password = undefined;
 
     return res.send({ user });
   } catch (err) {
