05b5300efd09efe15313e9411c16d0797fe2ef94
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2018-04-11 22:50:12 -0300

    add authentication verification

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
+router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
   return res.send({ ok: 'true' });
