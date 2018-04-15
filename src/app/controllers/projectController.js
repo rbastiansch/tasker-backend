ce493016e01d8f2894dab7a60e2b52353a40a2ac
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2018-04-15 00:19:19 -0300

    add route to create project

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/project');
+
 const router = express.Router();
 router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
   return res.send({ ok: 'true' });
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, description, tasks } = req.body;
+
+    const project = await Project.create({ title, description, user: req.userId });
+
+    return res.send({ project });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send({ error: 'Error creating new project '});
+  }
 });
 
 module.exports = app => app.use('/projects', router);
