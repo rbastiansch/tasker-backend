579c324c1a47ea8ac621396c41bbead0636503ef
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2018-04-22 22:36:09 -0300

    set headers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use( function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('OK');
 });
